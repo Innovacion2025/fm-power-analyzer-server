2042a040b302fbb51f4dc715fa5bbe15889d5f90
Innovacion2025 <fmelectrodo.innovacion@gmail.com>
2026-03-12 09:55:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const server = http.createServer(app);
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 // ====== CREDENCIALES DEL EDITOR ======
 const ADMIN_USER = "fmadmin";
 const ADMIN_PASS = "ClaveTemporal123!";
